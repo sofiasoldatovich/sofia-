1adc3bd97c587bbc295176ad73ce04887f7aadee
sofiasoldatovich <63725830+sofiasoldatovich@users.noreply.github.com>
2020-04-15 09:29:09 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 let mySprite = sprites.create(img`
-. . . . . . . . 3 3 3 3 3 3 . . 
-. . . . . . . . 3 3 3 3 3 3 3 . 
-. . . . . 3 3 3 3 3 3 3 3 3 3 3 
-. . . . 3 3 3 3 3 3 3 3 3 3 3 3 
-. . . . 3 3 3 3 3 3 3 3 3 3 3 3 
-. . . . 3 3 3 3 3 3 3 3 3 3 3 3 
-. . . . 3 3 3 3 3 3 3 3 3 3 3 3 
-. . . . 3 3 3 3 3 3 3 3 3 3 3 3 
-. . . . 3 3 3 3 3 3 3 3 3 3 3 3 
-. . . . 3 3 3 3 3 3 3 3 3 3 3 3 
-. . . . 3 3 3 3 3 3 3 3 3 3 3 3 
-. . . . . 3 3 3 3 3 3 3 3 3 3 3 
-. . . . . 3 3 3 3 3 3 3 3 3 3 . 
-. . . . . . . . 3 3 3 3 3 3 . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . c c f f f . . . . . . . . . . . . . . 
+. . . . . . . . . . . . c d d b b f . . . . . . . . . . . . . . 
+. . . . . . . . . . . c d d b b f . . . . . . . . . . . . . . . 
+. . . . . . . . . . f c c b b c f . . . . . . . . . . . . c c c 
+. . . . f f f f f f c c c c c c f f . . . . . . . . . c c b b c 
+. . f f b b b b b b b b b b b b b c f f f . . . . . c d b b c . 
+f f b b b b b b b b b c b c b b b b c c c f f . . c d d b b f . 
+f b c b b b b b f f b b c b c b b b c c c c c f f f d b b f . . 
+f b b b 1 1 1 1 f f 1 b c b c b b b c c c c c c c b b b c f . . 
+. f b 1 1 1 1 1 1 1 1 b b b b b b c c c c c c c c c b c c f . . 
+. . f c c c 3 3 c c 1 1 b b b b c c c c c c c c f f f b b c f . 
+. . . f c 1 3 1 c 1 1 1 b b b c c c c c b d b c . . . f b b f . 
+. . . . f 3 3 c 1 1 1 c b b b f d d d d d c c . . . . . f b b f 
+. . . . . f f 1 1 1 1 f b d b b f d d c c . . . . . . . . f f f 
+. . . . . . . c c c c c f b d b b f c . . . . . . . . . . . . . 
+. . . . . . . . . . . . . f f f f f . . . . . . . . . . . . . . 
 `, SpriteKind.Player)
